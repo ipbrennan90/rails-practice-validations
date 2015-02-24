de439f57310d8d4bafac6e80668e6680b29bd900
Ian Brennan <ian.brennan@icloud.com>
2015-02-24 11:52:11 -0700

    savin stuff

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,5 @@
 class Organization < ActiveRecord::Base
+  validates :name, presence: true
 
 
 
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,4 @@
 class Person < ActiveRecord::Base
-
-
-
+  validates :first_name,:date_of_birth,:last_name, presence: true
+  validates :date_of_birth, uniqueness: true
 end
